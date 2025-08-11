ab8b0cd16770981bc7c3659388d31755edf30478
gwongilnam <nimpa3201@naver.com>
2025-08-12 02:10:32 +0900

    feat: @Query,레포지토리 메소드에 쿼리 정의하기

--- src/main/java/study/data_jpa/repository/MemberRepository.java
+++ src/main/java/study/data_jpa/repository/MemberRepository.java
@@ -7,13 +7,17 @@ import study.data_jpa.entity.Member;
 
 import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member,Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
     // 메서드 이름으로 쿼리 생성
-    List<Member> findByUsernameAndAgeGreaterThan(String username,int age);
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
     List<Member> findTop3HelloBy();
 
-    @Query( name ="Member.findByUsername")
+    @Query(name = "Member.findByUsername")
     List<Member> findByUsername(@Param("username") String username);
+
+    @Query(" select  m from Member  m where m.username = :username and m.age = :age") // 이름 없는 NamedQuery, 애플리케이션 로딩시 문법 오류 검사
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 
 }

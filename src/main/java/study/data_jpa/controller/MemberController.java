924fe3bc92c16ebdb9955b7757fbda28c384d725
gwongilnam <nimpa3201@naver.com>
2025-08-14 11:05:09 +0900

    feat: web 확장 - 페이징과 정렬

--- src/main/java/study/data_jpa/controller/MemberController.java
+++ src/main/java/study/data_jpa/controller/MemberController.java
@@ -2,10 +2,14 @@ package study.data_jpa.controller;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.data_jpa.dto.MemberDto;
 import study.data_jpa.entity.Member;
 import study.data_jpa.repository.MemberRepository;
 
@@ -17,19 +21,29 @@ public class MemberController {
     private final MemberRepository memberRepository;
 
     @GetMapping("/members/{id}")
-    public String findMember(@PathVariable("id")Long id){
+    public String findMember(@PathVariable("id") Long id) {
         Member member = memberRepository.findById(id).get();
         return member.getUsername();
     }
 
     @GetMapping("/members2/{id}")
-    public String findMember(@PathVariable("id") Member member){
+    public String findMember(@PathVariable("id") Member member) {
         return member.getUsername();
     }
 
-    @PostConstruct
-    public void init(){
-        memberRepository.save(new Member("member1"));
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5, sort = "username") Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+        return page.map(m -> new MemberDto(m.getId(), m.getUsername(), null));
     }
+
+
+    @PostConstruct
+    public void init() {
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
+    }
+
 
 }

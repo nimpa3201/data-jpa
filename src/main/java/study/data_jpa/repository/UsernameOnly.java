9d84b6e1fe955baa7405be5c03853476364cc9ac
gwongilnam <nimpa3201@naver.com>
2025-08-21 16:31:54 +0900

    feat: Projections

---
+++ src/main/java/study/data_jpa/repository/UsernameOnly.java
@@ -0,0 +1,10 @@
+package study.data_jpa.repository;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public interface UsernameOnly  {
+
+    @Value("#{target.username + ' ' +target.age}")
+    String getUsername();
+
+}
